8d602cb2faa7c8efc7f03850d58d815491d83927
[Shakir-zahid] <[shakirzahid191@gmail.com]>
2021-08-10 12:59:39 +0500

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,37 @@
 import'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:spicy_destop_invoic_app/home/homePage.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 
-void main(){
+Future<void> main() async {
+  await Hive.initFlutter();
+  await Hive.openBox<String>('tableBox');
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+// class MyApp extends StatelessWidget {
+//   const MyApp({ Key? key }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: HomePage(),
+//     );
+//   }
+// }
+
+
+class MyApp extends StatefulWidget {
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  Box<String> tableBox;
 
   @override
   Widget build(BuildContext context) {
